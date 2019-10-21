4ee91d62d309735544d1a938e5d0eb9f0d1b7faa
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-21 12:09:01 -0400

    all winning conditions are functional

--- script.js
+++ script.js
@@ -53,6 +53,9 @@ class Color {
 //create new color to start game with red
 let currentColor = new Color('red', 'one');
 
+//create variable to keep track of number of plays
+let playCount = 0;
+
 //if you click inside game space color automatically switches
 gameSpace.addEventListener('click', function(e) {
   currentColor.switchColor();
@@ -62,47 +65,56 @@ gameSpace.addEventListener('click', function(e) {
 //add event listeners for each variable so when they are clicked their color changes
 a3.addEventListener('click', function(e) {
   e.preventDefault();
-  a3.style.background = currentColor.color;
-  checkWinner();
+  a3.style.background = currentColor.color; //change background color to current players color
+  playCount++; //increment play count
+  checkWinner(); //check to see if there is a winner
 });
 a2.addEventListener('click', function(e) {
   e.preventDefault();
   a2.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 a1.addEventListener('click', function(e) {
   e.preventDefault();
   a1.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 b3.addEventListener('click', function(e) {
   e.preventDefault();
   b3.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 b2.addEventListener('click', function(e) {
   e.preventDefault();
   b2.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 b1.addEventListener('click', function(e) {
   e.preventDefault();
   b1.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 c3.addEventListener('click', function(e) {
   e.preventDefault();
   c3.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 c2.addEventListener('click', function(e) {
   e.preventDefault();
   c2.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 c1.addEventListener('click', function(e) {
   e.preventDefault();
   c1.style.background = currentColor.color;
+  playCount++;
   checkWinner();
 });
 
@@ -111,20 +123,43 @@ c1.addEventListener('click', function(e) {
  */
 
 checkWinner = () => {
-  //straight across a3, b3, c3
-  if (a3.style.background === b3.style.background && b3.style.background === c3.style.background) {
-    console.log('straight across a3, b3, c3');
-  }
-  //straight across a2, b2, c2
-  if (a2.style.background === b2.style.background && b2.style.background === c2.style.background) {
-    console.log('straight across a2, b2, c2');
-  }
-  //straight across a1, b1, c1
-  if (a1.style.background === b1.style.background && b1.style.background === c1.style.background) {
-    console.log('straight across a1, b1, c1');
-  }
-  //straight down a3, a2, a1
-  if (a3.style.background === a2.style.background && a2.style.background === a1.style.background) {
-    console.log('straight down a3 a2 a1');
+  //check winner only if at least 5 plays have been made
+  if (playCount >= 5) {
+    //straight across a3, b3, c3
+    if (a3.style.background === b3.style.background && b3.style.background === c3.style.background) {
+      console.log('straight across a3, b3, c3');
+    }
+    //straight across a2, b2, c2
+    if (a2.style.background === b2.style.background && b2.style.background === c2.style.background) {
+      console.log('straight across a2, b2, c2');
+    }
+    //straight across a1, b1, c1
+    if (a1.style.background === b1.style.background && b1.style.background === c1.style.background) {
+      console.log('straight across a1, b1, c1');
+    }
+    //straight down a3, a2, a1
+    if (a3.style.background === a2.style.background && a2.style.background === a1.style.background) {
+      console.log('straight down a3 a2 a1');
+    }
+    //straight down b3, b2, b1
+    if (b3.style.background === b2.style.background && b2.style.background === b1.style.background) {
+      console.log('straight down b3 b2 b1');
+    }
+    //straight down b3, b2, b1
+    if (b3.style.background === b2.style.background && b2.style.background === b1.style.background) {
+      console.log('straight down b3 b2 b1');
+    }
+    //straight down c3, c2, c1
+    if (c3.style.background === c2.style.background && c2.style.background === c1.style.background) {
+      console.log('straight down c3 c2 c1');
+    }
+    //diagonals a3, b2, c1
+    if (a3.style.background === b2.style.background && b2.style.background === c1.style.background) {
+      console.log('diagonals a3, b2, c1');
+    }
+    //diagonals c3, b2, a1
+    if (c3.style.background === b2.style.background && b2.style.background === a1.style.background) {
+      console.log('diagonals c3, b2, a1');
+    }
   }
 };
