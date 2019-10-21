6696ce029fe660c0db74a4edba572623125a38a7
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-21 11:02:46 -0400

    created variables for each square

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
  * Tyler Eikenberg
  * *****************************
  */
+
+//gather all separate squares in individual variables
+const a3 = document.querySelector('#a3');
+const b3 = document.querySelector('#b3');
+const c3 = document.querySelector('#c3');
+const a2 = document.querySelector('#a2');
+const b2 = document.querySelector('#b2');
+const c2 = document.querySelector('#c2');
+const a1 = document.querySelector('#a1');
+const b1 = document.querySelector('#b1');
+const c1 = document.querySelector('#c1');
